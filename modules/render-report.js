ac085d1acab5436b319ca1706c96d6022553472c
Luke McFarlane <luke.mcfarlane@fluxfederation.com>
2023-02-02 20:40:01 +1300

    Render issues for each epic

--- modules/render-report.js
+++ modules/render-report.js
@@ -1,3 +1,52 @@
-export default function renderReport(report) {
-  console.log('rendering report:', report.epics)
+function renderEpic(epic) {
+  const cols = ['Key', 'Summary', 'Status', 'Story Points', 'Time Spent']
+
+  const containerDiv = document.createElement('div')
+  const h2El = document.createElement('h2')
+  h2El.textContent = epic.key
+
+  containerDiv.appendChild(h2El)
+
+  const tableEl = document.createElement('table')
+  const tHeadEl = document.createElement('thead')
+
+  const thEls = cols.map(col => {
+    const thEl = document.createElement('th')
+    thEl.textContent = col
+    return thEl
+  })
+
+  thEls.forEach(thEl => tHeadEl.appendChild(thEl))
+  tableEl.appendChild(tHeadEl)
+
+  const tBodyEl = document.createElement('tbody')
+
+  epic.issues.forEach(issue => {
+    const trEl = document.createElement('tr')
+    ;[
+      issue.key,
+      issue.summary,
+      issue.status,
+      issue.storyPoints,
+      issue.timeSpent
+    ].forEach(val => {
+      const tdEl = document.createElement('td')
+      tdEl.textContent = val
+      trEl.appendChild(tdEl)
+    })
+
+    tBodyEl.appendChild(trEl)
+  })
+
+  tableEl.appendChild(tBodyEl)
+
+  containerDiv.appendChild(tableEl)
+
+  document.querySelector('body').appendChild(containerDiv)
 }
+
+export default function renderReport(report) {
+  report.epics.forEach(renderEpic)
+}
+
+
